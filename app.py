0ea3ab976176e601656fafdf6f05449dc23514c0
bluzes <45350779+bluzes@users.noreply.github.com>
2023-06-18 11:57:51 +0000

    logging

--- app.py
+++ app.py
@@ -22,6 +22,9 @@ import sys
 
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+
 # Flask API endpoint
 # API_URL = 'http://localhost:5000' # No flask required atm
 
